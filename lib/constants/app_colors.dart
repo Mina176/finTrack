5fdcd4ed9dc85da5d11f8bd4871bc8becec07f87
Mina Ayman <mtrmena176@gmail.com>
2026-01-22 22:46:42 +0200

    add gradient colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,8 +1,16 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const kPrimaryColor = Color(0xFF13EC5B);
 const kBackgroundColor = Color(0xFF0D1B12);
-const kCardColor = Color(0xFF0D1B12);
+const kCardColor = Color(0xFF193322);
 const kTitleColor = Color(0xFFFFFFFF);
 const kSubtitleColor = Color(0xFF92C9A4);
 const kButtonLabelColor = Color(0xFF102216);
+
+final gradientColors = const LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF0D2018), Colors.black],
+);
--- lib/features/onboarding/presentation/splash_screen.dart
+++ lib/features/onboarding/presentation/splash_screen.dart
@@ -1,7 +1,10 @@
+import 'package:fintrack/constants/app_colors.dart';
 import 'package:fintrack/constants/app_sizes.dart';
 import 'package:fintrack/constants/text_styles.dart';
-import 'package:fintrack/features/onboarding/presentation/onboarding_content.dart';
+import 'package:fintrack/features/onboarding/presentation/onboarding_card.dart';
+import 'package:fintrack/router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({
@@ -12,29 +15,33 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF0D1B12),
-      body: Center(
-        child: TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0.0, end: 1.0),
-          curve: Curves.linear,
-          duration: Duration(milliseconds: 1200),
-          builder: (context, opacity, child) {
-            return Opacity(
-              opacity: opacity,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  OnboardingContent(
-                    width: 120,
-                    height: 120,
-                    title: 'FinTrack',
-                    subTitle: 'Master your money',
-                    icon: Icons.account_balance_wallet,
-                    borderRadius: Sizes.p24,
-                  ),
-                ],
-              ),
-            );
-          },
+      body: Container(
+        decoration: BoxDecoration(gradient: gradientColors),
+        child: Center(
+          child: TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0.0, end: 1.0),
+            curve: Curves.linear,
+            duration: Duration(milliseconds: 1200),
+            builder: (context, opacity, child) {
+              return Opacity(
+                opacity: opacity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    OnboardingCard(
+                      width: 120,
+                      height: 120,
+                      title: 'FinTrack',
+                      subTitle: 'Master your money',
+                      icon: Icons.account_balance_wallet,
+                      borderRadius: Sizes.p24,
+                    ),
+                  ],
+                ),
+              );
+            },
+            onEnd: () => context.go(AppRouter.kOnBoardingScreen),
+          ),
         ),
       ),
     );
