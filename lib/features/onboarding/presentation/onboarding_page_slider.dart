3cd1e5c67e79e44f33d7826a28b9921eb66b3838
Mina Ayman <mtrmena176@gmail.com>
2026-01-22 22:48:08 +0200

    complete onboarding screen

---
+++ lib/features/onboarding/presentation/onboarding_page_slider.dart
@@ -0,0 +1,66 @@
+import 'package:fintrack/constants/app_sizes.dart';
+import 'package:fintrack/features/onboarding/presentation/onboarding_card.dart';
+import 'package:fintrack/features/onboarding/presentation/slider_index.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class OnboardingPageSlider extends StatelessWidget {
+  const OnboardingPageSlider({
+    super.key,
+    required this.onSlide,
+    required this.currentPage,
+    required this.pageController,
+  });
+
+  final void Function(int)? onSlide;
+  final int currentPage;
+  final PageController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.58,
+          child: PageView(
+            onPageChanged: onSlide,
+            controller: pageController,
+            children: [
+              OnboardingCard(
+                width: 180,
+                height: 180,
+                title: 'Financial Freedom',
+                subTitle:
+                    'Track your spending, save more, and achieve financial freedom.',
+                icon: FontAwesomeIcons.chartSimple,
+                borderRadius: 128,
+              ),
+              OnboardingCard(
+                width: 180,
+                height: 180,
+                title: 'Smart Categorization',
+                subTitle:
+                    'Automatically categorize your transactions for effortless tracking.',
+                icon: FontAwesomeIcons.chartPie,
+                borderRadius: 32,
+              ),
+              OnboardingCard(
+                width: 180,
+                height: 180,
+                title: 'Master your Monthly Budget',
+                subTitle:
+                    'Set your first budget and start saving today! Track every expense and watch your savings grow.',
+                icon: Icons.account_balance_wallet_rounded,
+                borderRadius: 32,
+              ),
+            ],
+          ),
+        ),
+        SliderIndex(
+          currentPage: currentPage,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -1,42 +1,78 @@
+import 'package:fintrack/constants/app_colors.dart';
 import 'package:fintrack/constants/app_sizes.dart';
-import 'package:fintrack/features/onboarding/presentation/onboarding_content.dart';
-import 'package:fintrack/features/onboarding/presentation/slider_carousel.dart';
+import 'package:fintrack/features/onboarding/presentation/onboarding_card.dart';
+import 'package:fintrack/features/onboarding/presentation/onboarding_page_slider.dart';
+import 'package:fintrack/features/onboarding/presentation/slider_index.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  int currentPage = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(Sizes.p12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('skip'),
-              Spacer(),
-              OnboardingContent(
-                width: 180,
-                height: 180,
-                title: 'Financial Freedom',
-                subTitle:
-                    'Track your spending, save more, and achieve financial freedom.',
-                icon: FontAwesomeIcons.chartSimple,
-                borderRadius: 128,
-              ),
-              gapH24,
-              SliderIndex(
-                currentPage: 0,
-              ),
-              Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Next'),
-              ),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: gradientColors,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(Sizes.p12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () => context.go('/home'),
+                  child: Text('skip'),
+                ),
+                Spacer(),
+                OnboardingPageSlider(
+                  currentPage: currentPage,
+                  pageController: _pageController,
+                  onSlide: (index) => setState(() {
+                    currentPage = index;
+                  }),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    currentPage < 2
+                        ? _pageController.nextPage(
+                            duration: Duration(milliseconds: 250),
+                            curve: Curves.linear,
+                          )
+                        : context.go('/home');
+                  }),
+                  child: currentPage < 2
+                      ? Text('Next')
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Get Started  '),
+                            Icon(
+                              Icons.arrow_forward_rounded,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ],
+                        ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
