4f5bba1b3a1a65b2fee4833a0a6984e73b1f5ec9
Mina Ayman <mtrmena176@gmail.com>
2026-01-22 22:45:17 +0200

    constants for routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,19 +1,27 @@
+import 'package:fintrack/features/home%20screen/presentation/home_screen.dart';
 import 'package:fintrack/features/onboarding/presentation/onboarding_screen.dart';
 import 'package:fintrack/features/onboarding/presentation/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
+  static const String kSplashScreen = '/';
+  static const String kOnBoardingScreen = '/onboarding';
+  static const String kHomeScreen = '/home';
   // GoRouter configuration
   static final router = GoRouter(
-    initialLocation: '/onboarding',
+    initialLocation: '/',
     routes: [
       GoRoute(
-        path: '/',
+        path: kSplashScreen,
         builder: (context, state) => SplashScreen(),
       ),
       GoRoute(
-        path: '/onboarding',
+        path: kOnBoardingScreen,
         builder: (context, state) => OnboardingScreen(),
+      ),
+      GoRoute(
+        path: kHomeScreen,
+        builder: (context, state) => HomeScreen(),
       ),
     ],
   );
