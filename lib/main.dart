36ccd0a5b3cded4b99e9d9d4ba27fce19801e0b7
Mina Ayman <mtrmena176@gmail.com>
2026-01-22 18:30:49 +0200

    apply Inter google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fintrack/features/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,17 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: buildTheme(Brightness.dark),
+
       home: OnBoarding(),
+    );
+  }
+
+  ThemeData buildTheme(Brightness brightness) {
+    final baseTheme = ThemeData(brightness: brightness);
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.interTextTheme(baseTheme.textTheme),
     );
   }
 }
