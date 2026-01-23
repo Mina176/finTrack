e9e17b45b91a84b2814b5776f105ad709d9ed95a
Mina Ayman <mtrmena176@gmail.com>
2026-01-23 18:24:29 +0200

    create user model

---
+++ lib/features/authentication/data/user_model.dart
@@ -0,0 +1,19 @@
+// user_model.dart
+class UserModel {
+  final String uid;
+  final String name;
+  final String email;
+
+  UserModel({
+    required this.uid,
+    required this.name,
+    required this.email,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
+}
