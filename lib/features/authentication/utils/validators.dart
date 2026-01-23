90b4efd48a2a3b967c5f5309db0473fb8794fb9b
Mina Ayman <mtrmena176@gmail.com>
2026-01-23 19:30:58 +0200

    validators

---
+++ lib/features/authentication/utils/validators.dart
@@ -0,0 +1,34 @@
+// lib/core/utils/validators.dart
+
+class Validators {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Invalid email address';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+    return null;
+  }
+
+  static String? validateConfirmPassword(String? password, String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please confirm password';
+    }
+    if (password != value) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+}
