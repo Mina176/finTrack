5c5c6ca32f878ae1b71fbf2107bc4f5b208a19ee
Mina Ayman <mtrmena176@gmail.com>
2026-01-23 23:20:43 +0200

    install riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
