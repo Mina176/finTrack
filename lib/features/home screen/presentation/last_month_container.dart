0afcacb30a005d0b8285e3fb57c01e64db53448f
Mina Ayman <mtrmena176@gmail.com>
2026-01-25 05:03:51 +0200

    adding isSaving variable

--- lib/features/home screen/presentation/last_month_container.dart
+++ lib/features/home screen/presentation/last_month_container.dart
@@ -1,28 +1,60 @@
+import 'package:fintrack/constants/app_sizes.dart';
 import 'package:fintrack/constants/text_styles.dart';
 import 'package:fintrack/theming/app_colors.dart';
 import 'package:fintrack/utils/get_hardcode.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LastMonthContainer extends StatelessWidget {
-  const LastMonthContainer({super.key, required this.isSaving});
-  final bool isSaving;
+  const LastMonthContainer({
+    super.key,
+    required this.savingPercentage,
+    this.isShrinked = false,
+  });
+  final int savingPercentage;
+  final bool isShrinked;
+
   @override
   Widget build(BuildContext context) {
+    bool isSaving = savingPercentage >= 0;
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.kButtonBorderColor,
+        color: isSaving
+            ? AppColors.kButtonBorderColor
+            : AppColors.knotSavingBackground,
         borderRadius: BorderRadius.circular(16),
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Text(
-          '+12% vs last month'.hardcoded,
-          textAlign: TextAlign.center,
-          style: TextStyles.buttonLabel.copyWith(
-            color: AppColors.kPrimaryColor,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          ),
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            isSaving
+                ? Icon(
+                    FontAwesomeIcons.arrowTrendUp,
+                    color: AppColors.kPrimaryColor,
+                    size: 12,
+                  )
+                : Icon(
+                    FontAwesomeIcons.arrowTrendDown,
+                    color: AppColors.knotSavingForeground,
+                    size: 12,
+                  ),
+            gapW8,
+            Text(
+              isShrinked
+                  ? '${savingPercentage.toString()}%'
+                  : '$savingPercentage% vs last month'.hardcoded,
+              textAlign: TextAlign.center,
+              style: TextStyles.buttonLabel.copyWith(
+                color: isSaving
+                    ? AppColors.kPrimaryColor
+                    : AppColors.knotSavingForeground,
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+              ),
+            ),
+          ],
         ),
       ),
     );
