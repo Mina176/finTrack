a6f3a9c9223f388e861b80336c233417baa3bf8e
Mina Ayman <mtrmena176@gmail.com>
2026-02-20 02:01:15 +0200

    conplete onboarding method

--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -1,19 +1,21 @@
+import 'package:fintrack/features/onboarding/data/onboarding_repository.dart';
 import 'package:fintrack/routing/app_route_enum.dart';
 import 'package:fintrack/theming/app_colors.dart';
 import 'package:fintrack/constants/app_sizes.dart';
 import 'package:fintrack/features/onboarding/presentation/onboarding_page_slider.dart';
 import 'package:fintrack/widgets/button_label_with_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class OnboardingScreen extends StatefulWidget {
+class OnboardingScreen extends ConsumerStatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
+  ConsumerState<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
-class _OnboardingScreenState extends State<OnboardingScreen> {
+class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
   int currentPage = 0;
   final PageController _pageController = PageController(initialPage: 0);
   @override
@@ -22,50 +24,52 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     super.dispose();
   }
 
+  void completeOnboarding() async {
+    await ref.read(onboardingRepositoryProvider).setOnboardingCompleted();
+    if (mounted) {
+      context.go(AppRoutes.signIn.path);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DecoratedBox(
-        decoration: BoxDecoration(
-          gradient: AppColors.darkGradientColors,
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(Sizes.p12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () => context.go('/home'),
-                  child: Text('skip'),
-                ),
-                Spacer(),
-                OnboardingPageSlider(
-                  currentPage: currentPage,
-                  pageController: _pageController,
-                  onSlide: (index) => setState(() {
-                    currentPage = index;
-                  }),
-                ),
-                Spacer(),
-                ElevatedButton(
-                  onPressed: () => setState(() {
-                    currentPage < 2
-                        ? _pageController.nextPage(
-                            duration: Duration(milliseconds: 250),
-                            curve: Curves.linear,
-                          )
-                        : context.go(AppRoutes.signIn.path);
-                  }),
-                  child: currentPage < 2
-                      ? Text('Next')
-                      : ButtonLabelWithIcon(
-                          label: 'Get Started',
-                          icon: Icons.arrow_forward_rounded,
-                        ),
-                ),
-              ],
-            ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(Sizes.p12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: completeOnboarding,
+                child: Text('skip'),
+              ),
+              Spacer(),
+              OnboardingPageSlider(
+                currentPage: currentPage,
+                pageController: _pageController,
+                onSlide: (index) => setState(() {
+                  currentPage = index;
+                }),
+              ),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () => setState(() {
+                  currentPage < 2
+                      ? _pageController.nextPage(
+                          duration: Duration(milliseconds: 250),
+                          curve: Curves.linear,
+                        )
+                      : completeOnboarding;
+                }),
+                child: currentPage < 2
+                    ? Text('Next')
+                    : ButtonLabelWithIcon(
+                        label: 'Get Started',
+                        icon: Icons.arrow_forward_rounded,
+                      ),
+              ),
+            ],
           ),
         ),
       ),
