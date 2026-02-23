a5a2894be112351ab6714d8887b155f8206a5710
Mina Ayman <mtrmena176@gmail.com>
2026-02-23 10:24:55 +0200

    DisplayAmount with text field instead of custom keypad

--- lib/features/add transaction/presentation/display_amount.dart
+++ lib/features/add transaction/presentation/display_amount.dart
@@ -1,41 +1,47 @@
 import 'package:fintrack/constants/text_styles.dart';
 import 'package:fintrack/controllers/keyboard_controller.dart';
+import 'package:fintrack/features/currency/logic/currency_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DisplayAmount extends ConsumerWidget {
   const DisplayAmount({
     super.key,
-    required this.amount,
+    required this.controller,
   });
-  final String amount;
+  final TextEditingController controller;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isKeypadVisible = ref.watch(keypadControllerProvider);
+    final currencyCode = ref.watch(currencyCodeProvider);
     return Column(
       children: [
-        const Text(
-          "Amount (USD)",
+        Text(
+          "Amount ($currencyCode)",
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.grey),
         ),
-        GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-            ref.read(keypadControllerProvider.notifier).show();
-          },
-          behavior: HitTestBehavior.opaque,
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.15,
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Text(
-                amount,
-                style: TextStyles.amount.copyWith(
-                  color: isKeypadVisible ? null : Colors.grey,
-                ),
-              ),
-            ),
+        TextField(
+          controller: controller,
+          keyboardType: const TextInputType.numberWithOptions(decimal: true),
+          textAlign: TextAlign.center,
+          style: TextStyles.amount,
+          showCursor: false,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            filled: false,
+            hintText: "0.00",
+            hintStyle: WidgetStateTextStyle.resolveWith((states) {
+              if (states.contains(WidgetState.focused)) {
+                return const TextStyle(
+                  color: Colors.black,
+                );
+              }
+              return const TextStyle(
+                color: Colors.grey,
+              );
+            }),
           ),
         ),
       ],
