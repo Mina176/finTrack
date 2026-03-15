dae93872cdef588316a0ff4eae67cce614ec9638
Mina176 <mtrmena176@gmail.com>
2026-03-15 23:38:25 +0200

    storage provider

---
+++ lib/utils/storage_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/experimental/persist.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_sqflite/riverpod_sqflite.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+part 'storage_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<Storage<String, String>> storage(Ref ref) async {
+  return JsonSqFliteStorage.open(
+    join(await getDatabasesPath(), 'fintrack.db'),
+  );
+}
