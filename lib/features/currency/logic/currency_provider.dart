a148d78f5f0785c58caed592a78478dc61e50ff0
Mina176 <mtrmena176@gmail.com>
2026-03-15 17:27:21 +0200

    upgrade currency provider

--- lib/features/currency/logic/currency_provider.dart
+++ lib/features/currency/logic/currency_provider.dart
@@ -1,5 +1,29 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final currencyCodeProvider = StateProvider<String>((ref) => 'USD');
+// Make sure this matches your file name!
+// If your file is called 'currency_provider.dart', this must be 'currency_provider.g.dart'
+part 'currency_provider.g.dart';
 
-final currencySymbolProvider = StateProvider<String>((ref) => '\$');
+@riverpod
+class CurrencyCode extends _$CurrencyCode {
+  @override
+  String build() {
+    return 'USD';
+  }
+
+  void setCurrencyCode(String newCode) {
+    state = newCode;
+  }
+}
+
+@riverpod
+class CurrencySymbol extends _$CurrencySymbol {
+  @override
+  String build() {
+    return '\$';
+  }
+
+  void setCurrencySymbol(String newSymbol) {
+    state = newSymbol;
+  }
+}
