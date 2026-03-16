7526604afc7822d83a79dde36a0709aae237dbd1
Mina176 <mtrmena176@gmail.com>
2026-03-16 20:20:47 +0200

    fix forgot password screen

--- lib/features/authentication/presentation/forgot_password_screen.dart
+++ lib/features/authentication/presentation/forgot_password_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fintrack/constants/app_sizes.dart';
 import 'package:fintrack/features/authentication/logic/auth_controller.dart';
+import 'package:fintrack/features/authentication/logic/auth_service.dart';
 import 'package:fintrack/features/authentication/logic/loading_state.dart';
 import 'package:fintrack/features/authentication/presentation/auth_field.dart';
 import 'package:fintrack/utils/validators.dart';
@@ -17,106 +18,100 @@ class ForgotPasswordScreen extends ConsumerStatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends ConsumerState<ForgotPasswordScreen> {
-  final _formKey = GlobalKey<FormState>();
+  final formKey = GlobalKey<FormState>();
   String? emailError;
   final _emailContoller = TextEditingController();
+  bool isLoading = false;
   void sendResetEmail() async {
-    setState(() {
-      emailError = null;
-    });
-    if (_formKey.currentState?.validate() ?? false) {
+    if (!formKey.currentState!.validate()) return;
+    try {
+      setState(() {
+        isLoading = true;
+        emailError = null;
+      });
       await ref
-          .read(authControllerProvider.notifier)
+          .read(authServiceProvider)
           .sendPasswordResetEmail(_emailContoller.text.trim());
+      setState(() => isLoading = false);
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Reset link sent! Please check your email.'),
+          ),
+        );
+        context.pop();
+      }
+    } catch (e) {
+      final validationError = Validators.validatePasswordWhenSignUp(
+        e.toString(),
+      );
+      if (validationError != null) {
+        setState(() => emailError = validationError);
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final authState = ref.watch(authControllerProvider);
-    final isLoading = authState.isLoading;
-
-    ref.listen(authControllerProvider, (previous, next) {
-      if (next.state == LoadingStateEnum.success) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text(
-              'Reset link has been sent.',
-            ),
-          ),
-        );
-        context.pop();
-      } else if (next.hasError) {
-        final error = next.errorMessage.toString();
-        final validationError = Validators.getResetError(error);
-        if (validationError != null) {
-          setState(() => emailError = validationError);
-        }
-      }
-    });
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: Sizes.kHorizontalPadding,
           ),
-          child: CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  spacing: 16,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      onPressed: () => context.pop(),
-                      icon: const Icon(Icons.arrow_back_ios_new),
-                    ),
-                    const OnboardingCard(
-                      icon: Icons.lock_reset_sharp,
-                      borderRadius: 50,
-                      width: 100,
-                      height: 100,
-                      title: 'Forgot Password?',
-                      subTitle:
-                          'Don\'t worry, it happens. Please enter the email address associated with your account.',
-                    ),
-                    Form(
-                      key: _formKey,
-                      child: TextFieldWithLabel(
-                        label: 'Email Address',
-                        hintText: 'you@example.com',
-                        errorText: emailError,
-                        controller: _emailContoller,
-                        validator: Validators.validateEmail,
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    spacing: 16,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const OnboardingCard(
+                        icon: Icons.lock_reset_sharp,
+                        borderRadius: 50,
+                        width: 100,
+                        height: 100,
+                        title: 'Forgot Password?',
+                        subTitle:
+                            'Don\'t worry, it happens. Please enter the email address associated with your account.',
                       ),
-                    ),
-                    const Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: Sizes.kVerticalPadding,
+                      Form(
+                        key: formKey,
+                        child: TextFieldWithLabel(
+                          label: 'Email Address',
+                          hintText: 'you@example.com',
+                          errorText: emailError,
+                          controller: _emailContoller,
+                          validator: Validators.validateEmail,
+                        ),
                       ),
-                      child: ElevatedButton(
-                        onPressed: isLoading ? null : sendResetEmail,
-                        child: isLoading
-                            ? const SizedBox(
-                                height: 20,
-                                width: 20,
-                                child: CircularProgressIndicator(
-                                  strokeWidth: 2,
-                                ),
-                              )
-                            : const Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text('Send Reset Link'),
-                                  Icon(Icons.arrow_forward_rounded),
-                                ],
-                              ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: Sizes.kVerticalPadding,
+                ),
+                child: ElevatedButton(
+                  onPressed: isLoading ? null : sendResetEmail,
+                  child: isLoading
+                      ? const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                          ),
+                        )
+                      : const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Send Reset Link'),
+                            Icon(Icons.arrow_forward_rounded),
+                          ],
+                        ),
                 ),
               ),
             ],
